90d3b27931fbd231da29e4f3d8a16b29ac0c1ec3
Nuku <ravagex30@gmail.com>
2022-03-31 08:40:23 +0000

    Added errorhandler for internal server error

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -295,5 +295,13 @@ def create_app(test_config=None):
         "message" : "Bad request"
       }), 400
 
+  @app.errorhandler(500)
+  def internal_server_error(error):
+      return jsonify({
+        "success" : False,
+        "error" : 500,
+        "message" : "Internal server error"
+      })
+
 
   return app
